52d497211b4e2126db38d5f5ce6b05fb584dd633
akmurugan <47816686+akmurugan@users.noreply.github.com>
2020-10-27 17:53:15 +0000

    main.tf file added

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstorageacc"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "centralindia"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "akmurugan"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "ak11224864/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
