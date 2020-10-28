90563315293b729d69a56b6976b24f90fa9d330f
akmurugan <47816686+akmurugan@users.noreply.github.com>
2020-10-28 05:38:19 +0000

    update

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
 
 terraform {
     backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstorageacc"
+        resource_group_name  = "tf_stgacc_rg"
+        storage_account_name = "tfstorageacc23"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
